6d60efead2d475b2ca7543b6236eb1509ee7a279
Eva Brechum <Brechum.Eva@gmail.com>
2021-10-18 18:12:21 -0500

    loops exercises updates

---
+++ js/break_and_continue.js
@@ -0,0 +1,29 @@
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+//     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+// var userNumber= Number(prompt("what is your number?"))
+// for(var x =1; x<50; x++){
+//     if(userNumber==x) {
+//         console.log("Number to skip is " + userNumber);
+//
+//     }
+//     if(x % 2 == 0 ){
+//     }
+//         console.log("Yikes! S")
+//     }
+//     if(x%2!== 0){
+//         console.log("here's odd number")
+// }
+var userNumber = Number(prompt("give me an odd number between 1 and 50?"))
+console.log("Number to skip is " + userNumber);
+
+  for(var i = 1; i<50; i++) {
+      if(i%2===0){
+          continue;
+      }
+      if(userNumber === i){
+         console.log("We are skipping " + i);
+      }
+      if(i%2!==0)
+          console.log("Yikes! Skipping " + i);
+  }
--- js/for_loops.js
+++ js/for_loops.js
@@ -7,3 +7,33 @@ function showMultiplicationTable(num) {
     }
     }
 showMultiplicationTable(7);
+
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between
+// 20 and 200 and output to the console whether each number is odd or even.
+//
+//
+var randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20
+if (randomNumber % 2 === 0) {
+    console.log(randomNumber + " is even")
+}else {
+    console.log(randomNumber + " is odd");
+}
+
+
+// Create a for loop that uses console.log to create the output shown below.
+for(var i = 1; i < 10; i++) {
+console.log(i.toString().repeat(i));
+}
+// var totalNumberOfRows = 9;
+// var output = "";
+// for (var i = 1; i <= totalNumberOfRows; i++){
+//     for(var j = 1; j <= i; j++){
+//         output += j + "";
+//     }
+//     console.log("");
+// }
+
+// Create a for loop that uses console.log to create the output shown below.
+for(var i= 100; i >= 5; i-= 5) {
+    console.log(i);
+}
