695647a8dbe6cf18698c036a6bad43334e719f4d
Eva Brechum <Brechum.Eva@gmail.com>
2021-10-20 09:44:34 -0500

    arrays exercises

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -36,21 +36,24 @@
 // Write a program that console.logs the numbers from 1 to 100.
 // But for multiples of three console.log “Fizz” instead of the number and for the multiples of five console.log “Buzz”.
 // For numbers which are multiples of both three and five console.log “FizzBuzz”.
- for(var i = 1;  i <=100; i++) {
-     let output = "";
-     if(i % 3 === 0 && i % 5===0){
-         output += "Fizzbuzz";
-     }else if (i % 3 === 0) {
-         output += "Fizz";
-     }else if (i % 5 === 0) {
-         output += "Buzz";
-     }else {
-         output = i;
-     }
-     console.log(output);
- }
+//  for(var i = 1;  i <=100; i++) {
+//      let output = "";
+//      if(i % 3 === 0 && i % 5===0){
+//          output += "Fizzbuzz";
+//      }else if (i % 3 === 0) {
+//          output += "Fizz";
+//      }else if (i % 5 === 0) {
+//          output += "Buzz";
+//      }else {
+//          output = i;
+//      }
+//      console.log(output);
+//  }
 
-
+function secondToLast(arr){
+     return arr[arr.length -2];
+}
+console.log(secondToLast(["first", "second", "third", "last"])
 
 
 
