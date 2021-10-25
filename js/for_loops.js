918025eafdcc2e72d4cd344eb9b9c54f4cb18bc2
Eva Brechum <Brechum.Eva@gmail.com>
2021-10-24 23:12:55 -0500

    working on the do-while loop portion of exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -7,7 +7,7 @@
 
     function showMultiplicationTable(num) {
         for (var i = 1; i <= 10; i++) {
-            console.log(num + "x" + i + "=" + (num * i));
+            console.log(num + " x " + i + " = " + (num * i));
         }
     }
     showMultiplicationTable(7);
