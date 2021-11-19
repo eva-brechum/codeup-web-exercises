859b942a852c418f2aca9f1abff37768dcdd17ea
Eva Brechum <Brechum.Eva@gmail.com>
2021-11-19 16:10:24 -0600

    jquery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,16 @@
 "use strict";
 
-console.log("This is linked");
-
+//
 // var contents = $('#paragraph1').html();
 // alert(contents);
 
-$('#paragraph1');
+
+// $('.codeup').css("border", "1px solid red");
+
+// $("li").css("font-size", "20px");
+
+// $("h1").css("background-color", "gold");
+//
+// $("p").css("background-color", "blue");
+//
+// $("li").css("background-color", "grey");
