881c893329668d528783ac644b7fa117a631a8ec
Eva Brechum <Brechum.Eva@gmail.com>
2021-11-29 19:30:58 -0600

    working on traversal exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,12 +4,20 @@ $(document).ready(function() {
 
     });
 //    BONUS
-//     $("dt").click(function() {
-//         $(this).toggleClass("highlight");
-//     });
+    $("dt").click(function() {
+        $(this).toggleClass("highlight");
+    });
     $("#highlightButton").click(function () {
         $("ul").each(function () {
             $(this).children().eq(3).toggleClass("highlight");
         });
     });
+    $("h3").click(function () {
+        $("ul").toggleClass("fontBold");
+    })
+    // $("ul").each(function() {
+    //     $(this).click(function() {
+    //         $(this).children().eq(0).toggleClass("ulParent")
+    //     });
+    // });
 });
