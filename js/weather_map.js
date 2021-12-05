2c706aaee85d6407eda3ef4b04f6fec652f91c55
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-05 17:52:28 -0600

    working on weather map project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,30 +1,40 @@
+"use strict";
 $(document).ready(function(){
 
 
-    mapboxgl.accessToken = MAPBOX_KEY
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-95.3698, 29.7604], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-    });
-
-    var Houston= new mapboxgl.Marker()
-        .setLngLat([-95.3698, 29.7604])
-        .addTo(map);
-
+    // mapboxgl.accessToken = MAPBOX_KEY
+    // const map = new mapboxgl.Map({
+    //     container: 'map', // container ID
+    //     style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    //     center: [-95.3698, 29.7604], // starting position [lng, lat]
+    //     zoom: 9 // starting zoom
+    // });
+    //
+    // var Houston= new mapboxgl.Marker()
+    //     .setLngLat([-95.3698, 29.7604])
+    //     .addTo(map);
+    //
     // var popup = new mapboxgl.Popup()
-    //     .setHTML(<h1>"Houston"</h1>)
+    //     .setHTML("<h1>Houston</h1>")
     //     .addTo(map);
 
+    //
+    // $.get("api.openweathermap.org/data/2.5/weather", {
+    //     APPID: OPEN_WEATHER_KEY,
+    //     lat: 29.7604,
+    //     lon: -95.3698
+    // })
 
-    $.get("api.openweathermap.org/data/2.5/weather", {
-        APPID: OPEN_WEATHER_KEY,
-        lat: 29.7604,
-        lon: -95.3698
+    // $("#find").click(function(){
+    // })
+
+    // Make API Call
+    $.get("https://api.openweathermap.org/data/2.5/weather", {
+                APPID: OPEN_WEATHER_KEY,
+                lat: 29.7604,
+            lon: -95.3698,
+        units: "imperial"
+        }).done(function(data) {
+        console.log(data);
     })
-
-    $("#find").click(function(){
-    })
-
 });
