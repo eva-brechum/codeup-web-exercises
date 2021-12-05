e640a524ed48509c06274b938495b6968cdbff23
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-05 15:14:51 -0600

    working on weather map project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,9 +1,30 @@
-"use strict";
+$(document).ready(function(){
+
+
     mapboxgl.accessToken = MAPBOX_KEY
     const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-95.3698, 29.7604], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-95.3698, 29.7604], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
 
+    var Houston= new mapboxgl.Marker()
+        .setLngLat([-95.3698, 29.7604])
+        .addTo(map);
+
+    // var popup = new mapboxgl.Popup()
+    //     .setHTML(<h1>"Houston"</h1>)
+    //     .addTo(map);
+
+
+    $.get("api.openweathermap.org/data/2.5/weather", {
+        APPID: OPEN_WEATHER_KEY,
+        lat: 29.7604,
+        lon: -95.3698
+    })
+
+    $("#find").click(function(){
+    })
+
+});
