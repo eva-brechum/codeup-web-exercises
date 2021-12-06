d41b8b06c6999574f3d670e4c027cc0f254bc8af
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-05 18:59:47 -0600

    working on weathermap project

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,17 +2,8 @@
 $(document).ready(function(){
 
 
-    // mapboxgl.accessToken = MAPBOX_KEY
-    // const map = new mapboxgl.Map({
-    //     container: 'map', // container ID
-    //     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    //     center: [-95.3698, 29.7604], // starting position [lng, lat]
-    //     zoom: 9 // starting zoom
-    // });
-    //
-    // var Houston= new mapboxgl.Marker()
-    //     .setLngLat([-95.3698, 29.7604])
-    //     .addTo(map);
+
+
     //
     // var popup = new mapboxgl.Popup()
     //     .setHTML("<h1>Houston</h1>")
@@ -35,6 +26,22 @@ $(document).ready(function(){
             lon: -95.3698,
         units: "imperial"
         }).done(function(data) {
-        console.log(data);
+        console.log("5 day forecast", data);
+        $("#weather_icon").attr("src", "http://openweathermap.org/img/w/04n.png")
+            // + data.daily[0].weather[0].icon
+        $("#weather_icon2").attr("src", " http://openweathermap.org/img/w/02d.png")
+        console.log(new Date(data.daily[0].dt * 1000));
     })
+
+    mapboxgl.accessToken = MAPBOX_KEY
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-95.3698, 29.7604], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
+
+    var Houston= new mapboxgl.Marker()
+        .setLngLat([-95.3698, 29.7604])
+        .addTo(map);
 });
