11a5f4d9fcdeed38d601524d7a51300ca6950922
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-06 15:12:11 -0600

    successfully added icons to the map project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,22 +1,7 @@
 "use strict";
 $(document).ready(function(){
 
-
-
-
-    //
-
-
-    //
-    // $.get("api.openweathermap.org/data/2.5/weather", {
-    //     APPID: OPEN_WEATHER_KEY,
-    //     lat: 29.7604,
-    //     lon: -95.3698
-    // })
-
-
-
-    // Make API Call
+    // Make API Call for weather info
     $.get("https://api.openweathermap.org/data/2.5/weather", {
                 APPID: OPEN_WEATHER_KEY,
                 lat: 29.7604,
@@ -26,10 +11,11 @@ $(document).ready(function(){
         console.log("5 day forecast", data);
         //to input weather icons//
 
-        $("#weather_icon").attr("src", "http://openweathermap.org/img/w/04n.png")
+
+        $("#forecast-icon").attr("src", "http://openweathermap.org/img/w/04n.png")
             // + data.daily[0].weather[0].icon + "png");
-        $("#weather_icon2").attr("src", " http://openweathermap.org/img/w/02d.png")
-        // console.log(new Date(data.daily[0].dt * 1000));
+        // $("#forecast-icon").attr("src", " http://openweathermap.org/img/w/02d.png")
+        // // console.log(new Date(data.daily[0].dt * 1000));
     })
 
 
@@ -41,24 +27,24 @@ $(document).ready(function(){
         zoom: 9 // starting zoom
     });
 
-    var houstonMarker= new mapboxgl.Marker()
-        .setLngLat([-95.3698, 29.7604])
-        .setDraggable(true)
-        .addTo(map);
+    // var houstonMarker= new mapboxgl.Marker()
+    //     .setLngLat([long,lat])
+    //     // .setDraggable(true)
+    //     .addTo(map);
 
     // var placeMarker = new mapboxgl.Marker({
     //     draggable:true
     // })
-    // placeMarker
+    // houstonMarker
     //     .setLngLat([lon, lat])
     //     .addTo(map);
 // WHEN THE USER CLICKS THE FIND BUTTON, THE MARKER WILL HIGHLIGHT THE USER CURRENT CITY
-    $("#find").click(function(e){
-        e.preventDefault();
-    })
-
-     houstonMarker.on("dragend", function () {
-         $("")
-     })
+//     $("#find").click(function(e){
+//         e.preventDefault();
+//     })
+//
+//      houstonMarker.on("dragend", function () {
+//          $("")
+//      })
 
 });
