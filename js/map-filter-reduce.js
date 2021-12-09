ffd6bec4e2414c017123b3e9ca121c3af2b132d5
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-09 09:41:11 -0600

    map filter reduce exercise updates

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,10 +39,10 @@ const users = [
 //     Use .filter to create an array of user objects where each user object
 //     has at least 3 languages in the languages array.
 
-let languageThree = users.filter(users => users.languages.length >= 3);
+let languageThree = users.filter((language) => language.languages.length > 2);
 console.log(languageThree);
 //     Use .map to create an array of strings where each element is a user's email address
-let userEmail = users.map(users => users.email)
+let userEmail = users.map(user => user.email)
 console.log(userEmail);
 
 // Use .reduce to get the total years of experience from the list of users.
@@ -54,16 +54,48 @@ let totalYears = users.reduce((total, person) => {
 },  0);
 
 console.log(totalYears);
+let averageExp = totalYears/users.length;
+console.log(averageExp);
 
 //     Use .reduce to get the longest email from the list of users.
-let longestEmail = users.reduce((currentEmail, longEmail) => {
-    if(currentEmail.length > longEmail.length) return currentEmail;
-    return longEmail
-    }, "")
-console.log(longestEmail);
+// let longestEmail = users.reduce((currentEmail, longEmail) => {
+//     if(currentEmail.length > longEmail.length) return currentEmail;
+//     return longEmail
+//     }, "")
+// console.log(longestEmail);
+
+let longestEmail =users.reduce((startLngth, addLetter) => {
+    if(startLngth.email.length > addLetter.email.length) {
+        return startLngth;
+    }else{
+        return addLetter;
+    }
+})
+console.log(longestEmail)
 
 //     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-let userName = users.reduce((total,users) =>  {
-    return total += `${users.name}`;},''
-);
-console.log(userName);
+// let userName = users.reduce((total,users) =>  {
+//     return total += `${users.name}`;},''
+// );
+// console.log(userName);
+
+let instructorName =users.reduce ((finalStr, user, index) =>
+{
+    if (index === users.length -1) {
+    return `${finalStr} ${user.name},`
+}else{
+    return `${finalStr} ${user.name},`
+}
+}, "Your instructors are:")
+    console.log(instructorName);
+
+// Bonus Use .reduce to get the unique list of languages from the list of users.
+let uniqueList =users.reduce((langList, user) => {
+    for(let lang of user.languages) {
+        if(!langList.includes(lang)) {
+            langList.push(lang);
+        }
+    }
+    return langList;
+},[])
+console.log(uniqueList)
