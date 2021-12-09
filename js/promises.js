1f7a5c5f8d7119b30004835e5b383b2e64166b40
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-09 17:02:37 -0600

    finished promises exercise and walkthrough

--- js/promises.js
+++ js/promises.js
@@ -1,22 +1,44 @@
+"use strict";
 const lastCommit = (userName) => {
 fetch(`https://api.github.com/users/${userName}/events`,{headers: {'Authorization': 'GITHUB_KEY'}})
     .then(response => response.json ())
     .then(data=> {
-        console.log(data[0].created_at);
         console.log(data);
-    })};
+        // console.log(data[0].created_at);
+        for (let event of data) {
+            if (event.type === "PushEvent") {
+                console.log(new Date(event.created_at));
+                break;
+            }
+        }
+    });
+}
 lastCommit("eva-brechum");
 
-const wait = (number) => {
-    return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve()
-        }, 1000)
-    });
-};
-// wait(1000). then(() => console.log())
+// const wait = (number) => {
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve()
+//         }, 1000)
+//     });
+// };
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+// const wait = (number) => new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve();
+//     })
+//         reject();
+//     }, 1000)
+// })
+
+const wait = (ms) => new Promise( resolve => {
+    setTimeout(resolve, ms);
+});
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
 
 
 
