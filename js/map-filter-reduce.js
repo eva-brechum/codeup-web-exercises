697e8a27b1ff3fcf31a7518b19b66166de801b6b
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-08 22:58:34 -0600

    completed map-filter-reduce exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -56,7 +56,7 @@ let totalYears = users.reduce((total, person) => {
 console.log(totalYears);
 
 //     Use .reduce to get the longest email from the list of users.
-let longestEmail = email.reduce((currentEmail, longEmail) => {
+let longestEmail = users.reduce((currentEmail, longEmail) => {
     if(currentEmail.length > longEmail.length) return currentEmail;
     return longEmail
     }, "")
@@ -64,6 +64,6 @@ console.log(longestEmail);
 
 //     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 let userName = users.reduce((total,users) =>  {
-    return total += `Your instructors are ${acc} ${userName},`,)
-});
-console.log(userString);
+    return total += `${users.name}`;},''
+);
+console.log(userName);
