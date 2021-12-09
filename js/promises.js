fae9fd685d10a5502156633d514ad625b7ca2c03
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-09 14:33:40 -0600

    promises exercise

---
+++ js/promises.js
@@ -0,0 +1,10 @@
+const lastCommit = (userName) => {
+fetch(`https://api.github.com/users/${userName}/events`,{headers: {'Authorization': 'GITHUB_KEY'}})
+    .then(response => response.json ())
+    .then(data=> console.log(data)
+    )};
+lastCommit("eva-brechum");
+
+
+
+
