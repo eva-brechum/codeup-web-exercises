279bcdf7fef41b0008c9cfd1cdc2918c8cf36a03
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-09 15:11:01 -0600

    compled how to handle API promise exercise

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,13 @@
 const lastCommit = (userName) => {
 fetch(`https://api.github.com/users/${userName}/events`,{headers: {'Authorization': 'GITHUB_KEY'}})
     .then(response => response.json ())
-    .then(data=> console.log(data)
-    )};
+    .then(data=> {
+        console.log(data[0].created_at);
+        console.log(data);
+    })};
 lastCommit("eva-brechum");
+
+// const wait = ()
 
 
 
